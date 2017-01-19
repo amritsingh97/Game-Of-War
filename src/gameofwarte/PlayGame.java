0dc195f33c9bbab0524ccfe90194285efc3d4ffb
ASUS <ASUS@10.250.12.164>
2017-01-19 13:37:19 -0500

    Changes made

---
+++ src/gameofwarte/PlayGame.java
@@ -0,0 +1,13 @@
+package gameofwarte;
+
+public class PlayGame {
+    
+    public static void main(String[] args) {
+        
+        GameOfWar game = new GameOfWar();
+        
+        game.playTheGame();
+        
+    }
+    
+}
